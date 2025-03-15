04bbfd301feed4260e596b4e9a0f447dbdb5c9da
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-03-15 20:03:18 -0300

    feat(mapper): add toEntity method for converting PatientRequestDTO to Patient entity

--- patient-service/src/main/java/com/pm/patientservice/mapper/PatientMapper.java
+++ patient-service/src/main/java/com/pm/patientservice/mapper/PatientMapper.java
@@ -1,7 +1,10 @@
 package com.pm.patientservice.mapper;
 
+import com.pm.patientservice.dto.PatientRequestDTO;
 import com.pm.patientservice.dto.PatientResponseDTO;
 import com.pm.patientservice.model.Patient;
+
+import java.time.LocalDate;
 
 public class PatientMapper {
     public static PatientResponseDTO toDTO(Patient patient) {
@@ -12,5 +15,16 @@ public class PatientMapper {
                 patient.getAddress(),
                 patient.getDateOfBirth().toString()
         );
+    }
+
+    public static Patient toEntity(PatientRequestDTO patientRequestDTO) {
+        Patient patient = new Patient();
+        patient.setName(patientRequestDTO.name());
+        patient.setEmail(patientRequestDTO.email());
+        patient.setAddress(patientRequestDTO.address());
+        patient.setDateOfBirth(LocalDate.parse(patientRequestDTO.dateOfBirth()));
+        patient.setRegisteredDate(LocalDate.parse(patientRequestDTO.registeredDate()));
+
+        return patient;
     }
 }
