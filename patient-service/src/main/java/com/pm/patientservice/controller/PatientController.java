24216af3e7491db68445c647c2652e422b85b136
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-03-15 19:31:17 -0300

    feat(controller): add PatientController for patient data retrieval

---
+++ patient-service/src/main/java/com/pm/patientservice/controller/PatientController.java
@@ -0,0 +1,24 @@
+package com.pm.patientservice.controller;
+
+import com.pm.patientservice.dto.PatientResponseDTO;
+import com.pm.patientservice.service.PatientService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/patients")
+public class PatientController {
+    private final PatientService patientService;
+
+    public PatientController(PatientService patientService) {
+        this.patientService = patientService;
+    }
+
+    @GetMapping
+    public List<PatientResponseDTO> getPatients() {
+        return patientService.getAllPatients();
+    }
+}
