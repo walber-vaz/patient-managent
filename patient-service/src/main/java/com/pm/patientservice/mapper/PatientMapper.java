1100afc8d87da21edea21be964f77541e44d8e90
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-03-15 19:21:14 -0300

    feat(mapper): add PatientMapper for converting Patient to PatientResponseDTO

---
+++ patient-service/src/main/java/com/pm/patientservice/mapper/PatientMapper.java
@@ -0,0 +1,16 @@
+package com.pm.patientservice.mapper;
+
+import com.pm.patientservice.dto.PatientResponseDTO;
+import com.pm.patientservice.model.Patient;
+
+public class PatientMapper {
+    public static PatientResponseDTO toDTO(Patient patient) {
+        return new PatientResponseDTO(
+                patient.getId().toString(),
+                patient.getName(),
+                patient.getEmail(),
+                patient.getAddress(),
+                patient.getDateOfBirth().toString()
+        );
+    }
+}
