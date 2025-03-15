f020b20b5120c74a371d820c5c11fc67d78c0d98
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-03-15 19:21:22 -0300

    feat(dto): create PatientResponseDTO for patient data transfer

---
+++ patient-service/src/main/java/com/pm/patientservice/dto/PatientResponseDTO.java
@@ -0,0 +1,10 @@
+package com.pm.patientservice.dto;
+
+public record PatientResponseDTO(
+        String id,
+        String name,
+        String email,
+        String address,
+        String dateOfBirth
+) {
+}
