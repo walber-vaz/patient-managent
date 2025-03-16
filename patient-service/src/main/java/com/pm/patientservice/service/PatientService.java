49929fab78f8fb6cb5e965674843e47ffc22c343
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-03-15 22:21:10 -0300

    feat(service): implement updatePatient method with validation and exception handling

--- patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -2,13 +2,19 @@ package com.pm.patientservice.service;
 
 import com.pm.patientservice.dto.PatientRequestDTO;
 import com.pm.patientservice.dto.PatientResponseDTO;
+import com.pm.patientservice.dto.PatientUpdateRequestDTO;
 import com.pm.patientservice.exceptions.EmailAlreadyExistsException;
+import com.pm.patientservice.exceptions.NoChangesDetectedException;
+import com.pm.patientservice.exceptions.PatientNotFoundException;
 import com.pm.patientservice.mapper.PatientMapper;
 import com.pm.patientservice.model.Patient;
 import com.pm.patientservice.repository.PatientRepository;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class PatientService {
@@ -29,11 +35,60 @@ public class PatientService {
             throw new EmailAlreadyExistsException("Email already exists " + patientRequestDTO.email());
         }
 
-        Patient savedPatient = patientRepository.save(
-                PatientMapper.toEntity(patientRequestDTO)
-        );
+        Patient savedPatient = patientRepository.save(PatientMapper.toEntity(patientRequestDTO));
 
 
         return PatientMapper.toDTO(savedPatient);
+    }
+
+    public PatientResponseDTO updatePatient(UUID patientId, PatientUpdateRequestDTO dto) {
+        Patient patient = patientRepository.findById(patientId).orElseThrow(() -> new PatientNotFoundException("Patient not found with id " + patientId));
+
+        boolean isModified = false;
+
+        if (dto.email() != null) {
+            if (!dto.email().equals(patient.getEmail())) {
+                if (patientRepository.existsPatientByEmail(dto.email())) {
+                    throw new EmailAlreadyExistsException("Email already exists " + dto.email());
+                }
+                patient.setEmail(dto.email());
+                isModified = true;
+            }
+        }
+
+        if (dto.name() != null && !dto.name().equals(patient.getName())) {
+            patient.setName(dto.name());
+            isModified = true;
+        }
+
+        if (dto.address() != null && !dto.address().equals(patient.getAddress())) {
+            patient.setAddress(dto.address());
+            isModified = true;
+        }
+
+        if (dto.dateOfBirth() != null) {
+            LocalDate dob = LocalDate.parse(dto.dateOfBirth());
+            if (!dob.equals(patient.getDateOfBirth())) {
+                patient.setDateOfBirth(dob);
+                isModified = true;
+            }
+        }
+
+        if (dto.registeredDate() != null) {
+            LocalDate regDate = LocalDate.parse(dto.registeredDate());
+            if (!regDate.equals(patient.getRegisteredDate())) {
+                patient.setRegisteredDate(regDate);
+                isModified = true;
+            }
+        }
+
+        if (!isModified) {
+            throw new NoChangesDetectedException("No changes detected. All provided values match existing data.");
+        }
+
+        patient.setUpdatedAt(LocalDateTime.now());
+        patient = patientRepository.save(patient);
+
+        return PatientMapper.toDTO(patient);
     }
 }
