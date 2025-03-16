96de580fc896f3bd16950769dbdc1654f958d322
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-03-16 11:54:55 -0300

    feat(api): implement pagination and sorting for getAllPatients method

--- patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -9,11 +9,16 @@ import com.pm.patientservice.exceptions.PatientNotFoundException;
 import com.pm.patientservice.mapper.PatientMapper;
 import com.pm.patientservice.model.Patient;
 import com.pm.patientservice.repository.PatientRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 @Service
@@ -24,10 +29,22 @@ public class PatientService {
         this.patientRepository = patientRepository;
     }
 
-    public List<PatientResponseDTO> getAllPatients() {
-        List<Patient> patients = patientRepository.findAll();
+    public List<PatientResponseDTO> getAllPatients(Integer page, Integer size, String orderBy, String direction) {
+        Set<String> validFields = Set.of("id", "name", "email", "dateOfBirth", "registeredDate", "createdAt", "updatedAt");
 
-        return patients.stream().map(PatientMapper::toDTO).toList();
+        if (!validFields.contains(orderBy)) {
+            orderBy = "createdAt";
+        }
+
+        Sort.Direction sortDirection = direction.equalsIgnoreCase("asc") ?
+                Sort.Direction.ASC :
+                Sort.Direction.DESC;
+
+        Sort sort = Sort.by(sortDirection, orderBy);
+        Pageable pageable = PageRequest.of(page, size, sort);
+        Page<Patient> patients = patientRepository.findAll(pageable);
+
+        return patients.map(PatientMapper::toDTO).toList();
     }
 
     public PatientResponseDTO createPatient(PatientRequestDTO patientRequestDTO) {
