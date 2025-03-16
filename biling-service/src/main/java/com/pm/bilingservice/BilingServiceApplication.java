a7bf96c61e8af0c8f67ccc35d8003e5be7a23ed1
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-03-16 11:59:10 -0300

    feat(init): initialize billing service with Spring Boot and Maven wrapper

---
+++ biling-service/src/main/java/com/pm/bilingservice/BilingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pm.bilingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BilingServiceApplication.class, args);
+	}
+
+}
